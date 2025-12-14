ddf172254112849c8919610e0638001d54bbcd30
adalbertobrant <adalbertobrant@gmail.com>
2025-12-14 17:11:06 -0300

    exemplo multiplicacao de matrizes

--- 1312/multiplicacao.cpp
+++ 1312/multiplicacao.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main() {
-    // declaração das matrizes mat, x e res 
     int mat[2][2] = { {1,2}, {3,4} };
     int x[2][2] = { {5,6}, {7,8} };
     int res[2][2] = {0}; // Inicializa a matriz resultado com zeros
 
-    // Método 1: Multiplicação manual das matrizes 
+    // Método 1: Multiplicação manual
     res[0][0] = (mat[0][0] * x[0][0]) + (mat[0][1] * x[1][0]);
     res[1][0] = (mat[1][0] * x[0][0]) + (mat[1][1] * x[1][0]);
     res[0][1] = (mat[0][0] * x[0][1]) + (mat[0][1] * x[1][1]);
@@ -27,8 +26,7 @@ int main() {
     // Método 2: Multiplicação com loops
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) { // como vimos existe uma inversão nos valores de i e j , daí a necessidade do k 
-                // observem as linhas 11 a 14 e vejam a necessidade do k em relação ao i e j.
+            for (int k = 0; k < 2; k++) {
                 res2[i][j] += mat[i][k] * x[k][j];
             }
         }
